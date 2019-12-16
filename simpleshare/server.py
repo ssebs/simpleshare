ef27ef9bb9a10373d51b4c647b56ba207b2e81e0
Sebastian Safari <ssebs@ymail.com>
2019-12-16 13:37:44 -0800

    try some more sock opts. for multicast

--- simpleshare/server.py
+++ simpleshare/server.py
@@ -13,9 +13,10 @@ def broadcast_info(my_ip, mcastip, fn, port, dport):
 
     # addrinfo = socket.getaddrinfo(mcastip, None)[0]
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ttl_bin = struct.pack('@i', 2)
+    ttl_bin = struct.pack('@i', 20)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-
+    s.bind((my_ip, port))
     for t in range(tries):  # run for 2 mins ( delay*tries=x, x/60=ans )
         data = (
             f"ip: {str(my_ip)}, filename: {fn}, dataport: {dport}"
